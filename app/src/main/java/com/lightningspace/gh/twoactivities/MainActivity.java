46509a8a8722d317cd85cd1c67183ede3103c390
Daniel-Sogbey <mathematics06physics@gmail.com>
2021-05-29 23:34:32 -0700

    init commit

---
+++ app/src/main/java/com/lightningspace/gh/twoactivities/MainActivity.java
@@ -0,0 +1,41 @@
+package com.lightningspace.gh.twoactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    public static final String EXTRA_MESSAGE = "com.lightningspace.gh.twoactivities.extra.MESSAGE";
+    private EditText mMessageEditText;
+    private TextView mReply;
+    public  static final int TEXT_REQUEST =1;
+    private TextView mReplyHead;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mMessageEditText = (EditText) findViewById(R.id.editText_main);
+        mReply = (TextView) findViewById(R.id.reply_message);
+       mReplyHead = (TextView) findViewById(R.id.reply_message);
+
+        mReply.setText(mReply.getText().toString());
+        mReplyHead.setText(mReplyHead.getText().toString());
+    }
+
+    public void launchSecondActivity (View view) {
+        Intent intent = new Intent(this,SecondActivity.class);
+        String message = mMessageEditText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivityForResult(intent, TEXT_REQUEST);
+        Log.d(TAG, "launchSecondActivity:Button CLICK! ");
+    }
+}
---
+++ app/src/main/java/com/lightningspace/gh/twoactivities/SecondActivity.java
@@ -0,0 +1,35 @@
+package com.lightningspace.gh.twoactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+    public static final String EXTRA_REPLY = "com.lightningspace.gh.twoactivities.MainActivity.extra.REPLY";
+    private TextView textView;
+    private EditText mReply;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        textView = (TextView) findViewById(R.id.text_message);
+        textView.setText(message);
+
+        mReply = (EditText) findViewById(R.id.reply_text);
+    }
+
+    public void returnReply (View view) {
+        String reply = mReply.getText().toString();
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY,reply);
+        setResult(RESULT_OK, replyIntent);
+        finish();
+    }
+}
